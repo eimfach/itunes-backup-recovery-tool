89ad10f601c59c6e30c35fe94080e0d54d8c2e37
eimfach <eimfach@me.com>
2016-07-05 12:45:51 +0200

    Fixed module conversion regressions

--- index.js
+++ index.js
@@ -1,63 +1,83 @@
-const fs = require('fs');
-const plist = require('simple-plist');
-const readChunk = require('read-chunk');
-const fileType = require('file-type');
+'use strict'
 
-const mimeTypes = [
+const fs = require('fs')
+const plist = require('simple-plist')
+const readChunk = require('read-chunk')
+const fileType = require('file-type')
+
+const api = function (injectOptions) {
+  const supportedMimeTypes = injectOptions.supportedMimeTypes
+  return {
+    checkInfoAccess (path) {
+      const filePath = path + '/Info.plist'
+      fs.accessSync(filePath, fs.F_OK)
+      return true
+    },
+    /*
+     * check if plist files exists and check if it's encrypted
+     */
+    getBackupInfo: function (path) {
+      // TODO: READ manifest.plist PROPERTY BackupKeyBag
+      return new Promise((resolve, reject) => {
+        const filePath = path + '/Info.plist'
+        plist.readFile(filePath, (err, data) => {
+          if (!err) {
+            resolve(data)
+          } else {
+            reject('Info.plist not readable.')
+          }
+        })
+      })
+    },
+    getSortedFileList: function (options) {
+      const path = options.path
+      const files = options.files
+      const mimeTypes = options.mimeTypes || supportedMimeTypes
+      const fileList = {}
+
+      files.forEach((file) => {
+        const filePath = path + '/' + file
+
+        // read 'magic number' bytes
+        const buffer = readChunk.sync(filePath, 0, 262)
+        const fileDescriptionObject = fileType(buffer)
+
+        // add to sorted list if a supported mime type was detected
+        if (fileDescriptionObject && fileDescriptionObject.mime) {
+          let mimeType = fileDescriptionObject.mime
+          if (mimeTypes.indexOf(mimeType) > -1) {
+            if (fileList[mimeType] === undefined) {
+              fileList[mimeType] = []
+            }
+            fileList[mimeType].push(filePath)
+          }
+        }
+      })
+
+      return fileList
+    },
+    copyFile: function (source, target) {
+      return new Promise(function (resolve, reject) {
+        var rd = fs.createReadStream(source)
+        rd.on('error', reject)
+        var wr = fs.createWriteStream(target)
+        wr.on('error', reject)
+        wr.on('finish', resolve)
+        rd.pipe(wr)
+      })
+    }
+  }
+}
+
+const supportedMimeTypes = [
   'image/png',
   'image/jpeg',
   'image/jpg',
   'video/quicktime',
-  'audio/m4a',
-];
+  'audio/m4a'
+]
 
-module.exports = {
-  checkInfoAccess(path) {
-    const filePath = path+'/Info.plist';
-    fs.accessSync(filePath, fs.F_OK);
-    return true;
-  },
-  /*
-   * check if plist files exists and check if it's encrypted
-   */
-  getBackupInfo: function(path) {
-    //TODO: READ files async and return Promise
-    //TODO: READ manifest.plist PROPERTY BackupKeyBag
-    const filePath = path+'/Info.plist';
-    let info = {};
-    if(fs.statSync(filePath)) {
-      info = plist.readFileSync(filePath);
-    }
-    return info;
-  },
-  getSortedFileList: function({path, files}) {
-    const fileList = {};
-    files.forEach((file) => {
-      const filePath = path+'/'+file;
-      // read 'magic number' bytes
-      const buffer = readChunk.sync(filePath, 0, 262);
-      const fileDescriptionObject = fileType(buffer);
-
-      // add to sorted list if a supported mime type was detected
-      if(fileDescriptionObject && fileDescriptionObject.mime) {
-        let mimeType = fileDescriptionObject.mime;
-        if(mimeTypes.hasOwnProperty(mimeType)) {
-          fileList[mimeType] = [filePath, ...fileList[mimeType] || []];
-        }
-      }
-      return mimeList;
-    });
-
-    return fileList;
-  },
-  copyFile: function(source, target) {
-      return new Promise(function(resolve, reject) {
-          var rd = fs.createReadStream(source);
-          rd.on('error', reject);
-          var wr = fs.createWriteStream(target);
-          wr.on('error', reject);
-          wr.on('finish', resolve);
-          rd.pipe(wr);
-      });
-  }
-}
+module.exports = Object.freeze({
+  default: api({ supportedMimeTypes }),
+  inject: api
+})
--- itunes-backup-tool.js
+++ itunes-backup-tool.js
@@ -1,99 +1,118 @@
 #!/usr/bin/env node
-'use strict';
+'use strict'
 
-const tool = require('./index.js')
-const uuid = require('node-uuid');
-const fs = require('fs');
+const tool = require('./index.js').default
+const uuid = require('node-uuid')
+const fs = require('fs')
 
-const EOL = require('os').EOL;
+const EOL = require('os').EOL
 
-const doCheck = process.argv[2] === 'check';
-const doCopy = process.argv[2] === 'copy';
-const path = process.argv[3];
-const dest = process.argv[4];
+const doCheck = process.argv[2] === 'check'
+const doCopy = process.argv[2] === 'copy'
+const path = process.argv[3]
+const dest = process.argv[4]
 
 const fileExt = {
   'image/png': '.png',
   'image/jpeg': '.jpeg',
   'image/jpg': '.jpg',
   'video/quicktime': '.mov',
-  'audio/m4a': '.m4a',
+  'audio/m4a': '.m4a'
 }
 
-function errorMsg(msg) {
-  return '\x1b[31m' + msg + '\x1b[0m';
+function errorMsg (msg) {
+  return '\x1b[31m' + msg + '\x1b[0m'
 }
-function infoMsg(msg) {
+function infoMsg (msg) {
   return '\x1b[34m' + msg + '\x1b[0m'
 }
 
-if(['-h', '--h', '-help', '--help'].indexOf(process.argv[2]) > -1 ) {
-  process.stdout.write('check [path]'+EOL+'copy [path] [destination]'+EOL);
-  process.exit(0);
-}
-if(!process.argv[2]) {
-  process.stderr.write(errorMsg('Missing action param!')+EOL);
-  process.exit(1);
-}
-if(!path) {
-  process.stderr.write(errorMsg('Missing read path param!')+EOL);
-  process.exit(1);
-}
-if(doCopy && !dest) {
-  process.stderr.write(errorMsg('Missing destination path param')+EOL);
-  process.exit(1);
-}
+function handleMainTask () {
+  process.stdout.write('-> Try to read files in path: ' + path + EOL)
 
-let files;
+  try {
+    files = fs.readdirSync(path)
+    process.stdout.write('-> Found ' + files.length + ' files.' + EOL)
+  } catch (e) {
+    process.stderr.write(errorMsg('Failed to read from this path!' + EOL))
+  }
 
-const info = tool.getBackupInfo(path);
-if(Object.keys(info).length > 0) {
-  process.stdout.write(infoMsg(info['Device Name'])+EOL);
-  process.stdout.write('IMEI: '+infoMsg(info['IMEI'])+EOL);
-  process.stdout.write('Date: '+ infoMsg(new Date(info['Last Backup Date'])) +EOL);
-  process.stdout.write('Product Type: '+ infoMsg(info['Product Type'])+EOL);
-  process.stdout.write('Product Version: '+ infoMsg(info['Product Version'])+EOL);
-}
+  process.stdout.write('-> Checking supported filetypes in this backup......' + EOL)
+  // read the actual types of the files via the 'magic number' bytes
+  const fileList = tool.getSortedFileList({path, files})
 
-process.stdout.write('-> Try to read files in path: '+path+EOL);
-
-try {
-  files = fs.readdirSync(path);
-  process.stdout.write('-> Found '+files.length+' files.'+EOL);
-} catch (e) {
-  process.stderr.write(errorMsg('Failed to read from this path!'+EOL))
-}
-
-process.stdout.write('-> Checking filetypes in this backup......'+EOL);
-// read the actual types of the files via the 'magic number' bytes
-const fileList = tool.getSortedFileList({path, files});
-
-if(doCheck) {
-  Object.keys(fileList).forEach((keyAsMime) => {
-    const filesOfType = fileList[keyAsMime].length;
-    process.stdout.write('-> ' + infoMsg(filesOfType) + ' files of type: ' + keyAsMime +EOL );
-  });
-  process.exit(0);
-}
-
-if(doCopy) {
-  process.stderr.write('-> Starting copy process (async) to '+dest+' ....'+EOL);
-
-  let copyPromises = [];
-  Object.keys(fileList).forEach((keyAsMime) => {
-    fileList[keyAsMime].forEach((filePath) => {
-      const promiseCopy = tool.copyFile(filePath, dest+'/'+uuid.v4()+fileExt[keyAsMime]);
-      copyPromises.push(promiseCopy);
+  if (doCheck) {
+    Object.keys(fileList).forEach((keyAsMime) => {
+      const filesOfType = fileList[keyAsMime].length
+      process.stdout.write('-> ' + infoMsg(filesOfType) + ' files of type: ' + keyAsMime + EOL)
     })
-  });
-  Promise.all(copyPromises)
-  .then(() => {
-    process.stdout.write('-> Copy successful'+EOL);
-    process.exit(0);
-  })
-  .catch((e) => {
-    process.stderr.write(errorMsg(e.message)+EOL);
-    process.exit(1);
-  });
+    process.exit(0)
+  } else if (doCopy) {
+    let destExists = false
 
+    try {
+      fs.statSync(dest)
+      destExists = true
+    } catch (e) {
+      // directory does not exist or is not readable
+    }
+    if (!destExists) {
+      fs.mkdirSync(dest)
+    }
+    process.stderr.write('-> Starting copy process to ' + dest + ' ....' + EOL)
+
+    let copyPromises = []
+    Object.keys(fileList).forEach((keyAsMime) => {
+      fileList[keyAsMime].forEach((filePath) => {
+        const promiseCopy = tool.copyFile(filePath, dest + '/' + uuid.v4() + fileExt[keyAsMime])
+        copyPromises.push(promiseCopy)
+      })
+    })
+    Promise.all(copyPromises)
+    .then(() => {
+      process.stdout.write('-> Copy successful' + EOL)
+      process.exit(0)
+    })
+    .catch((e) => {
+      process.stderr.write(errorMsg(e.message) + EOL)
+      process.exit(1)
+    })
+  } else {
+    process.stderr.write(errorMsg('No main task specified.') + EOL)
+  }
 }
+
+let files
+
+if (['-h', '--h', '-help', '--help'].indexOf(process.argv[2]) > -1) {
+  process.stdout.write('check [path]' + EOL + 'copy [path] [destination]' + EOL)
+  process.exit(0)
+}
+if (!process.argv[2]) {
+  process.stderr.write(errorMsg('Missing action param!') + EOL)
+  process.exit(1)
+}
+if (!path) {
+  process.stderr.write(errorMsg('Missing read path param!') + EOL)
+  process.exit(1)
+}
+if (doCopy && !dest) {
+  process.stderr.write(errorMsg('Missing destination path param') + EOL)
+  process.exit(1)
+}
+
+tool.getBackupInfo(path)
+.then((info) => {
+  process.stdout.write(infoMsg(info['Device Name']) + EOL)
+  process.stdout.write('IMEI: ' + infoMsg(info['IMEI']) + EOL)
+  process.stdout.write('Date: ' + infoMsg(new Date(info['Last Backup Date'])) + EOL)
+  process.stdout.write('Product Type: ' + infoMsg(info['Product Type']) + EOL)
+  process.stdout.write('Product Version: ' + infoMsg(info['Product Version']) + EOL)
+  handleMainTask()
+})
+.catch((err) => {
+  let info = infoMsg('Info.plist seems not to exist in this Backup. It is possible that this Backup is incomplete.' + EOL)
+  process.stdout.write(errorMsg(err) + EOL)
+  process.stdout.write(info)
+  handleMainTask()
+})
